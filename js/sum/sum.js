e328bc7d832c4e6c09e6075fdb2afbcc4d4ad47f
Atru8729 <a.truskauskas1@gmail.com>
2021-05-24 20:55:09 +0300

    tests

--- js/sum/sum.js
+++ js/sum/sum.js
@@ -1,5 +1,20 @@
 function sum(a, b) {
-    return a + b;
-};
+    if (typeof a !== 'number' ||
+        typeof b !== 'number' ||
+        isNaN(a) ||
+        isNaN(b)) {
+        return false;
+    }
+
+    return b + a;
+}
 
 module.exports = sum;
+
+
+//const sum = (a, b) => a + b;
+
+//const fullName = (firstName, lastName) => firstName + ' ' + lastName;
+
+//const average = list => list.reduce((t, s) => t + s, 0) / list.length;
+
